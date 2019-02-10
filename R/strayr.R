97636179fb2c352562dea2f8c17eaf64451e03fb
Matt Cowgill <mattcowgill@gmail.com>
2019-02-10 17:41:40 +1100

    add `fuzzy_strayr()`

--- R/strayr.R
+++ R/strayr.R
@@ -8,8 +8,24 @@ strayr <- function(x, to = "state_abbr"){
 
   matched_abbr <- names(state_dict[match(dat, tolower(state_dict))])
 
-  return <- state_table[[to]][match(matched_abbr, state_table$state_abbr)]
+  ret <- state_table[[to]][match(matched_abbr, state_table$state_abbr)]
 
-  return
+  ret
+}
+
+#' @importFrom stringdist amatch
+#' @export
+fuzzy_strayr <- function(x, to = "state_abbr", max_dist = 0.4, method = "jw"){
+
+  dat <- gsub("[^[:alpha:] ]", "", x)
+
+  dat <- tolower(dat)
+
+  matched_abbr <- names(state_dict[stringdist::amatch(dat, tolower(state_dict), method = method, maxDist = max_dist)])
+
+  ret <- state_table[[to]][match(matched_abbr, state_table$state_abbr)]
+
+  ret
+
 }
 
