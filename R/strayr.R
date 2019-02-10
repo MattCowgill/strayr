99c87954041a98e5d1d064252b21ca78b1bfbc9d
Matt Cowgill <mattcowgill@gmail.com>
2019-02-10 21:40:15 +1100

    strayr() is now a wrapper for exacty_strayr() and fuzzy_strayr()

--- R/strayr.R
+++ R/strayr.R
@@ -1,6 +1,24 @@
 #' @export
 
-strayr <- function(x, to = "state_abbr"){
+strayr <- function(x, to = "state_abbr", fuzzy_match = TRUE, max_dist = 0.4, method = "jw"){
+  if(!is.logical(fuzzy_match)){
+    stop("`fuzzy_match` argument must be either `TRUE` or `FALSE`")
+  }
+
+  if(fuzzy_match){
+    ret <- fuzzy_strayr(x, to = to, max_dist = max_dist, method = method)
+  } else {
+    ret <- exact_strayr(x, to = to)
+  }
+
+  ret
+
+}
+
+
+#' @export
+
+exact_strayr <- function(x, to = "state_abbr"){
 
   dat <- state_string_tidy(x)
 
